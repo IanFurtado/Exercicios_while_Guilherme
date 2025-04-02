3af9fc60f062a8976e6bb9e766a99e43f16110f2
IanFurtado <sapocarnificina@gmail.com>
2025-04-02 11:06:28 -0300

    Upload numeros

---
+++ Exercicios/numeros.java
@@ -0,0 +1,29 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class numeros {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        byte contador = 0;
+        byte acumuladorp = 0;
+        byte acumuladori = 0;
+
+        do {
+        contador++;
+        System.out.println("Digite o " + contador + "º número");    
+        int resposta = entrada.nextInt();
+        
+        if (resposta % 2 == 0) {
+            acumuladorp++;
+        } else {
+            acumuladori++;
+        }
+        
+        } while (contador <= 9);
+
+        System.out.println("Total de números pares: " + acumuladorp + "\nTotal de números impares: " + acumuladori);
+        entrada.close();
+    }
+}
